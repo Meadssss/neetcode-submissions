77bd97eab9127aaf4771ca67bcad8bafc249472c
Meadssss <meademsujay12@gmail.com>
2026-09-06 09:14:50 +0100

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #1. we need to make it so that for every unique index, that we can return an array that contains the values, and then go through that array adn return that array. 
+        count ={}
+        freq = [[] for i in range(len(nums)+1)]
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0 ) 
+
+        for num,cnt in count.items(): 
+            freq[cnt].append(num) 
+
+        res = [] 
+        for i in range(len(freq) - 1,0,-1):
+            for num in freq[i]:
+                res.append(num) 
+                if len(res) == k: 
+                    return res 
+
+
+
+    
+
+
+        
+            
+
+
+
+
+        
