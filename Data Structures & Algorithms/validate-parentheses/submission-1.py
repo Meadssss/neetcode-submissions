080de7cf15400b60b049584fb2629578947b0a77
Meadssss <meademsujay12@gmail.com>
2026-09-07 11:51:52 +0100

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        #takes around 7 mins 
+        Q = [] 
+        matching = { ")": "(", "}": "{", "]": "["}
+        for i in s:
+            if i in matching: 
+                if Q and Q[-1] == matching[i]:
+                    Q.pop()
+                else:
+                    return False
+
+            else:
+                Q.append(i) 
+
+        return True if not Q else False 
+
+            
+
+
+        
